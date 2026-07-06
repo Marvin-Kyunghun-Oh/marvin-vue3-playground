0637e203eb3a3bb60aacca1e295f58e0690fbd8d
FRANC-OH_HOME\franc <france1988@naver.com>
2026-07-06 19:39:07 +0900

    12. Composition API   - 반응형, 라이프사이클 훅 API 테스트

---
+++ src/lectures/12. CompositionAPI/App_CompositionAPI.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h2>반응형 메시지</h2>
+    <p>{{ reactiveMessage }}</p>
+    <button @click="addReactiveMessage">Add Message</button>
+     <h2>일반 메시지</h2>
+     <p>{{ normalMessage }}</p>
+     <button @click="addNormalMessage">Add Message</button>
+  </div>
+</template>
+
+<script>
+  import { ref, isRef, onMounted, onBeforeMount } from 'vue'
+
+  export default {
+    setup() {
+      console.log('setup() is called');
+
+      // 반응형 : ref()를 통한 반응형 테스트
+      const reactiveMessage = ref('Hello, Reactive');
+      const addReactiveMessage = () => {
+        reactiveMessage.value += '!'
+      };
+      //console.log('reactiveMessage is ref:', isRef(reactiveMessage)); // true
+
+      // 반응형 : 일반 문자열을 통한 반응형 테스트
+      let normalMessage = "Hello, Normal";
+      const addNormalMessage = () => {
+        normalMessage += '!'
+      };
+      //console.log('normalMessage is ref:', isRef(normalMessage)); // false
+
+      // 라이프사이클 훅
+      onBeforeMount(() => {
+        console.log('onBeforeMount() is called');
+      })
+      onMounted(() => {
+        console.log('onMounted() is called');
+      })
+
+      return {
+        reactiveMessage,
+        normalMessage,
+        addReactiveMessage,
+        addNormalMessage,
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
