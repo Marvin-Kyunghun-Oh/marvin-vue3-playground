3b0afb6aae9d395d426c8a91f808c20b88a7748b
FRANC-OH_HOME\franc <france1988@naver.com>
2026-07-07 16:19:33 +0900

    20. 디렉티브 - v-once, v-text 실험.

---
+++ src/lectures/20. 디렉티브/App_derective.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>v-text</h1>
+    <p v-text="msg"></p>
+    <p>{{ msg }}</p>
+    <p v-text="htmlStr"></p>
+    <p v-html="htmlStr"></p>
+  </div>
+  <hr/>
+
+  <div>
+    <h1>v-once</h1>
+    <p v-once>{{ msg }}</p>
+    <button @click="msgChange">msgChange</button>
+  </div>
+  <hr/>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup () {
+    // v-text, v-html
+    let msg = ref('Hi~');
+    const htmlStr = ref('<strong>안녕</strong>');
+    
+    // v-once 테스트 - v-once는 변경 안됨.
+    const msgChange = () => {
+      msg.value += "!";
+    };
+
+    
+    return {
+      msg,
+      htmlStr,
+      msgChange,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
